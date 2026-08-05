331239d8738f5bcfb002ec23e9c401a859528fc0
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-08-06 01:50:08 +0300

    add more statuses

--- cogs/misc_msgs.py
+++ cogs/misc_msgs.py
@@ -37,6 +37,12 @@ class MiscMessages(V2BotCog):
         else:
             await inter.response.send_message(file=File("./medihha/spoon/spoon.png"))
 
+    @commands.slash_command(
+        name="opensource", description="me when free and open source software: 👍"
+    )
+    async def open_source_slash_command(self, inter: ApplicationCommandInteraction):
+        await inter.response.send_message("https://github.com/fluffykn1ght/v2-bot :3")
+
 
 def setup(bot: V2Bot):
     bot.add_cog(MiscMessages(bot))
