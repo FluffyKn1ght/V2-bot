e8a3dd1c723a6283cffd53809544992e9e7343cd
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-08-05 19:36:06 +0300

    add secrets.json.template

--- cogs/opensource.py
+++ cogs/opensource.py
@@ -1,9 +1,16 @@
+from disnake.ext import commands
+from disnake.interactions import ApplicationCommandInteraction
+
 from cogs.v2cog import V2BotCog
 from main import V2Bot
 
 
 class OpenSourceSlashCommand(V2BotCog):
-    pass
+    @commands.slash_command(
+        name="opensource", description="me when free and open source software: 👍"
+    )
+    async def open_source_slash_command(self, inter: ApplicationCommandInteraction):
+        await inter.response.send_message("https://github.com/fluffykn1ght/v2-bot :3")
 
 
 def setup(bot: V2Bot):
