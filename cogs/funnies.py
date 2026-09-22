6034bdb2e38bd59893bb462e3cfd541040f36dfd
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-09-22 21:39:24 +0300

    i can't tell you how much i like bread

--- cogs/funnies.py
+++ cogs/funnies.py
@@ -13,7 +13,7 @@ class Funnies(V2BotCog):
             return
 
         for text in self.bot.config["funnies"]:
-            if msg.content in text.split(","):
+            if msg.content.lower() in text.split(","):
                 funny_response = self.bot.config["funnies"][text]
 
                 if type(funny_response) is str:
